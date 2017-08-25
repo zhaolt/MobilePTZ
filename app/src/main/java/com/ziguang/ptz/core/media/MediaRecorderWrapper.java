908e895f7ffcb46a7f15109920a57e93618f778b
zhaolt <liangtai0035@sina.cn>
2017-08-25 18:22:07 +0800

    add media recorder

--- app/src/main/java/com/ziguang/ptz/core/media/MediaRecorderWrapper.java
+++ app/src/main/java/com/ziguang/ptz/core/media/MediaRecorderWrapper.java
@@ -1,6 +1,12 @@
 package com.ziguang.ptz.core.media;
 
+import android.hardware.Camera;
 import android.media.MediaRecorder;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.view.Surface;
+
+import java.io.IOException;
 
 /**
  * Created by zhaoliangtai on 2017/8/24.
@@ -8,19 +14,78 @@ import android.media.MediaRecorder;
 
 public class MediaRecorderWrapper {
 
+    private static final String TAG = MediaRecorderWrapper.class.getSimpleName();
 
     private MediaRecorder mMediaRecorder;
 
-    private MediaRecorderWrapper() {
-        mMediaRecorder = new MediaRecorder();
+    private static boolean isLollipop = false;
+
+    private MediaRecorderWrapper(MediaRecorder mediaRecorder) {
+        mMediaRecorder = mediaRecorder;
     }
 
 
-    public static MediaRecorderWrapper setUpMediaRecord(Object camera, int videoWidth,
+    public static MediaRecorderWrapper setUpMediaRecord(Camera camera, int videoWidth,
                                                         int videoHeight, int fps, int bitRate,
-                                                        int rotation, String outputFilePath) {
+                                                        int rotation) {
         MediaRecorderWrapper result = null;
+        MediaRecorder mediaRecorder = new MediaRecorder();
+        if (camera == null) {
+            isLollipop = true;
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        } else {
+            isLollipop = false;
+            mediaRecorder.setCamera(camera);
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        }
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mediaRecorder.setVideoEncodingBitRate(bitRate);
+        mediaRecorder.setVideoFrameRate(fps);
+        mediaRecorder.setVideoSize(videoWidth, videoHeight);
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mediaRecorder.setOrientationHint(rotation);
+        result = new MediaRecorderWrapper(mediaRecorder);
         return result;
     }
 
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public Surface getSurface() {
+        if (mMediaRecorder != null) {
+            return mMediaRecorder.getSurface();
+        }
+        return null;
+    }
+
+    public void startRecordingVideo(String outputFilePath) {
+        if (null == mMediaRecorder) {
+            throw new NullPointerException("MediaRecorder is NULL!");
+        }
+        mMediaRecorder.setOutputFile(outputFilePath);
+        try {
+            mMediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMediaRecorder.start();
+    }
+
+    public void releaseRecorder() {
+        if (null != mMediaRecorder) {
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+        }
+    }
+
+    public void stopRecordingVideo() {
+        if (null == mMediaRecorder) {
+            throw new NullPointerException("MediaRecorder is NULL!");
+        }
+        mMediaRecorder.stop();
+        mMediaRecorder.reset();
+    }
 }
